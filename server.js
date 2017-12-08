20923004ba64614008a2cfa6778ab228062186b3
Lukas Marx <marxlukas@googlemail.com>
2017-12-08 13:24:36 +0100

    removed fs import

--- server.js
+++ server.js
@@ -3,27 +3,21 @@ require('zone.js/dist/zone-node');
 
 const express = require('express');
 const ngExpressEngine = require('@nguniversal/express-engine').ngExpressEngine;
-const fs = require('fs');
 
-const {
-    ServerAppModuleNgFactory,
-    LAZY_MODULE_MAP
-} = require('./dist-server/main.bundle');
+const { ServerAppModuleNgFactory, LAZY_MODULE_MAP } = require('./dist-server/main.bundle');
 
 const app = express();
 
-const {
-    provideModuleMap
-} = require('@nguniversal/module-map-ngfactory-loader');
+const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
 
 const provider = provideModuleMap(LAZY_MODULE_MAP);
 
 app.engine(
-    'html',
-    ngExpressEngine({
-        bootstrap: ServerAppModuleNgFactory,
-        providers: [provider]
-    })
+  'html',
+  ngExpressEngine({
+    bootstrap: ServerAppModuleNgFactory,
+    providers: [provider]
+  })
 );
 
 app.set('view engine', 'html');
@@ -33,12 +27,12 @@ app.use(express.static(__dirname + '/assets', { index: false }));
 app.use(express.static(__dirname + '/dist', { index: false }));
 
 app.get('/*', (req, res) => {
-    console.time(`GET: ${req.originalUrl}`);
-    res.render('./dist/index', {
-        req: req,
-        res: res
-    });
-    console.timeEnd(`GET: ${req.originalUrl}`);
+  console.time(`GET: ${req.originalUrl}`);
+  res.render('./dist/index', {
+    req: req,
+    res: res
+  });
+  console.timeEnd(`GET: ${req.originalUrl}`);
 });
 
 app.listen(process.env.PORT || 8080, () => {});
